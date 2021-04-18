1155567bf816fd7c98b4bf68f474a00a3dd7d434
LeeSeungHun <58622039+superstarMann@users.noreply.github.com>
2021-04-18 15:50:49 +0900

    setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloProvider } from '@apollo/client/react';
+import './styles/styles.css'
+import { client } from './apollo';
 
 ReactDOM.render(
   <React.StrictMode>
+ <ApolloProvider client={client}>
     <App />
+  </ApolloProvider>,
   </React.StrictMode>,
   document.getElementById('root')
 );
