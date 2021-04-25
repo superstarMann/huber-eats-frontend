ec695d926f1621d4f6f6a66934005d5636c3b66b
LeeSeungHun <58622039+superstarMann@users.noreply.github.com>
2021-04-25 15:46:32 +0900

    Do not forget input:
    : dont miss

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,41 +1,66 @@
 import React from 'react';
+import { useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
+import { useForm } from 'react-hook-form';
+import { FormError } from '../components/form-error';
+
+const LOGIN_MUTATION = gql`
+ mutation TestMutation($email: String!, $password: String!){
+   login(input:{
+     email: $email,
+     password: $password
+   }) {
+     ok
+     token
+     error
+   }
+ }
+`;
+
+interface ILoginForms{
+  email?:string;
+  password?:string;
+}
 
 export const Login = () => {
-    return<span>Login</span>
+  const {register, getValues, errors, handleSubmit} = useForm<ILoginForms>()
+  const [loginMutation] = useMutation(LOGIN_MUTATION);
+  const onSubmit = () => {
+    console.log(getValues())
+  }
+
+    return <div className="h-screen flex items-center justify-center bg-gray-800">
+      <div className="bg-white w-full max-w-lg pt-7 pb-7 rounded-lg text-center">        
+          <h3 className='text-2xl text-gray-800'>Log In</h3>
+          <form 
+             onSubmit={handleSubmit(onSubmit)} 
+             className='grid gap-3 mt-5 px-5'>
+            <input 
+              ref={register({required: 'Email is required', minLength: 10})} 
+              required 
+              name='email' 
+              type="email" placeholder='Email' 
+              className='input'/>
+                 {errors.email?.message && (
+                   <FormError errorMessage={errors.email?.message}/>
+                   )}
+            <input 
+              ref={register({required: 'Password is required', minLength: 10})} 
+              required 
+              name='password' 
+              type="password" 
+              placeholder='Password' 
+              className='input'/>
+              {errors.password?.message && (
+                   <FormError errorMessage={errors.password?.message}/>
+                   )}
+              {errors.password?.type === 'minLength' && (
+                   <FormError errorMessage='Password must be more than 10 chars'/>
+              )}
+              <button className='mt-3 btn'>
+                Login
+              </button>
+          </form>
+      </div>
+      </div>
 } 
-/*
-<div>
-<h1>Logged Out</h1>
-<form onSubmit={handleSubmit(onSubmit, onInvalid)}>
-  <div>
-    <input
-      ref={register({
-        required: "This is required",
-        pattern: /^[A-Za-z0-9._%+-]+@gmail.com$/,
-      })}
-      name="email"
-      type="email"
-      placeholder="email"
-    />
-    {errors.email?.message && (
-      <span className="font-bold text-red-600">
-        {errors.email?.message}
-      </span>
-    )}
-    {errors.email?.type === "pattern" && (
-      <span className="font-bold text-red-600">Only gmail allowed</span>
-    )}
-  </div>
-  <div>
-    <input
-      ref={register({ required: true })}
-      name="password"
-      type="password"
-      required
-      placeholder="password"
-    />
-  </div>
-  <button className="bg-yellow-300 text-white">Submit</button>
-</form>
-</div>
-*/
