a4eb20f5971303d7f579dd4d3f60745aa5519f84
LeeSeungHun <58622039+superstarMann@users.noreply.github.com>
2021-05-12 22:43:00 +0900

    how to make btn

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import { useMutation, gql, ApolloError } from '@apollo/client';
+import { useMutation, gql } from '@apollo/client';
 import { useForm } from 'react-hook-form';
 import { FormError } from '../components/form-error';
 import { loginMutation, loginMutationVariables } from '../__generated__/loginMutation';
+import huberLogo from '../images/eats-logo.svg';
+import { Button } from '../components/button';
+import { Link } from 'react-router-dom';
 
 
 const LOGIN_MUTATION = gql`
@@ -21,7 +24,7 @@ interface ILoginForms{
 }
 
 export const Login = () => {
-  const {register, getValues, errors, handleSubmit, watch} = useForm<ILoginForms>()
+  const {register, getValues, errors, handleSubmit, formState} = useForm<ILoginForms>({mode: "onChange"})
   const onCompleted = (data: loginMutation) =>{
     const {login :{ok,token}} =data
     if(ok){
@@ -46,18 +49,20 @@ export const Login = () => {
   };
   
 
-    return <div className="h-screen flex items-center justify-center bg-gray-800">
-      <div className="bg-white w-full max-w-lg pt-7 pb-7 rounded-lg text-center">        
-          <h3 className='text-2xl text-gray-800'>Log In</h3>
+    return <div className="h-screen flex items-center flex-col mt-10 lg:mt-28">
+      <div className='w-full max-w-screen-sm flex flex-col items-center px-5'>
+      <img src={huberLogo} className='w-52 mb-10'/>
+      <h4 className='w-full text-left text-2xl mb-5 font-medium'>돌아오신 것을 환영합니다</h4>
           <form 
              onSubmit={handleSubmit(onSubmit)} 
-             className='grid gap-3 mt-5 px-5'>
+             className='grid gap-3 mt-5 w-full'>
             <input 
               ref={register({required: 'Email is required', minLength: 10})} 
               required 
               name='email' 
-              type="email" placeholder='Email' 
-              className='input'/>
+              type="email" placeholder='이메일 또는 휴대폰 번호' 
+              className='input transition-colors'
+              />
                  {errors.email?.message && (
                    <FormError errorMessage={errors.email?.message}/>
                    )}
@@ -74,11 +79,15 @@ export const Login = () => {
               {errors.password?.type === 'minLength' && (
                    <FormError errorMessage='Password must be more than 10 chars'/>
               )}
-              <button className='mt-3 btn'>
-                {loading ? 'Loading....' : 'Log In'}
-              </button>
-              {loginMutationResult?.login.error && <FormError errorMessage={loginMutationResult.login.error}/>}
+              <Button canClick={formState.isValid} loading={loading} actionText={"Log in"}/>
+              {loginMutationResult?.login.error && (<FormError errorMessage={loginMutationResult.login.error}/>)}
           </form>
-      </div>
-      </div>
+          <div>
+            New to Huber?{""}
+            <Link to="/create-account" className=" text-lime-600 hover:underline">
+              Create an Account
+            </Link>
+          </div>
+          </div>
+          </div>
 } 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: { //extends 무조건 쓰기(교체가 아니라 확장이기 때문)
+      colors:{
+        lime: colors.lime,
+      }
+    },
   },
   variants: {
     extend: {},
