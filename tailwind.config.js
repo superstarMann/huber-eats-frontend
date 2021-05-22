a1556dd0e3105b1a6a04e45251c7bb2d69d4c5dc
LeeSeungHun <58622039+superstarMann@users.noreply.github.com>
2021-05-22 15:26:25 +0900

    awd

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  purge: [],
+  purge: ['./src/**/*.tsx',],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: { //extends 무조건 쓰기(교체가 아니라 확장이기 때문)
