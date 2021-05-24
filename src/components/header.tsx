21a9c4711568f997787dfec466b2e5cf862af8e3
LeeSeungHun <58622039+superstarMann@users.noreply.github.com>
2021-05-24 16:59:10 +0900

    start

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import huberLogo from '../images/eats-logo.svg';
 
-export const Header = () => <header className="bg-red-500 py-4">
-     <div className='w-full max-w-screen-2xl bg-yellow-500 mx-auto'>
-         <img src={huberLogo} className="w-52 mb-10" alt="Huber Eats" />
-         I'm Header
-         </div>        
-    </header>
+export const Header = () => (
+     <header className="py-4">
+       <div className="w-full max-w-screen-xl mx-auto">
+         <img src={huberLogo} className="w-52 mb-10" alt="Nuber Eats" />
+         im the header
+       </div>
+     </header>
+   );
